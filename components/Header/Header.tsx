b8011ad09997ac616360e8dcc669a2ad9fbb8cb1
David Crawford <crawford@mail.gvsu.edu>
2023-06-03 13:06:59 -0400

    Added test meta tag

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -24,10 +24,31 @@ export const Header = () => {
     { href: '/about', children: 'About' },
   ];
 
+  const meta = {
+    title: 'David Crawford – Software Developer',
+    description: `Software Developer, writer, artist, creator.`,
+    image:
+      'https://media.licdn.com/dms/image/C5616AQFq00SLyxc58g/profile-displaybackgroundimage-shrink_350_1400/0/1630334856455?e=1676505600&v=beta&t=gccUn2DOokwI0SnxDjCu-KGz5xPsnkfN1s9TYi-f5yI',
+    type: 'website',
+  };
+
   return (
     <header
       aria-label="Site Header"
       className="font-sans antialiased bg-[#212224] fixed w-full z-50">
+      <title>Grand Rapids Developer</title>
+      <meta name="robots" content="follow, index" />
+      <meta content={meta.description} name="description" />
+      <meta property="og:type" content={meta.type} />
+      <meta property="og:site_name" content="David Crawford" />
+      <meta property="og:description" content={meta.description} />
+      <meta property="og:title" content={meta.title} />
+      <meta property="og:image" content={meta.image} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@GrandRapidsDev" />
+      <meta name="twitter:title" content={meta.title} />
+      <meta name="twitter:description" content={meta.description} />
+      <meta name="twitter:image" content={meta.image} />
       <Layout />
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div
