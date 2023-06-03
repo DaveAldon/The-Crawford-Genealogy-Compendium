33608a123fe5bfb8a77e34085f7caba4493a3aa3
David Crawford <crawford@mail.gvsu.edu>
2023-06-03 13:58:18 -0400

    Updated meta tags

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -50,15 +50,16 @@ export const Header = (props: HeaderProps) => {
       <title>{meta.title}</title>
       <meta name="robots" content="follow, index" />
       <meta content={meta.description} name="description" />
-      <meta name="title" property="og:type" content={meta.type} />
+      <meta property="og:type" content={meta.type} />
       <meta property="og:site_name" content={meta.title} />
       <meta property="og:description" content={meta.description} />
-      <meta property="og:title" content={meta.title} />
+      <meta name="title" property="og:title" content={meta.title} />
       <meta
         property="og:url"
         content={`https://crawfordlineage.com/${props.slug}`}
       />
       <meta name="image" property="og:image" content={meta.image} />
+      <meta property="og:image" content={meta.image} />
       <meta name="author" content="David Crawford" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@GrandRapidsDev" />
